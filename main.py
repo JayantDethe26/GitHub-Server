e239e4b26c9026def29b55be388d92919d1197d8
JayantDethe26 <dethejayant@gmail.com>
2026-02-23 14:11:51 +0530

    changed port

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def ping() -> str:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     mcp.run(
-        transport="streamable-http",
+        transport="sse",
         host="0.0.0.0",
         port=port,
     )
